8a0c995cd0647a5d7f63e07c1aa9c4b5c1f75b2d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-30 15:06:03 +0600

    performance optimization

--- ps-approach/count-char.js
+++ ps-approach/count-char.js
@@ -1,3 +1,15 @@
+function isAlphaNumericCharacter(char) {
+  const code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A-Z)
+    !(code > 96 && code < 123)
+  )
+    // lower alpha (a-z)
+    return false;
+  return true;
+}
+
 function countChar(str) {
   const lowerCasedStr = str.toLowerCase();
   const count = {};
@@ -10,7 +22,8 @@ function countChar(str) {
 
   // for (i = 0; i < lowerCasedStr.length; i++) {
   for (const char of lowerCasedStr) {
-    if (/[^a-zA-Z0-9]/g.test(char)) continue; // Note: Continue because it's inside a loop!
+    // if (/[^a-zA-Z0-9]/g.test(char)) continue; // Note: Continue because it's inside a loop!
+    if (!isAlphaNumericCharacter(char)) continue;
     count[char] = ++count[char] || 1; // Remark: Increment first then assign!
   }
 
