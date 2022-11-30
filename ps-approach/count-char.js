dbbf47ed49c396e7a3a94dbcffe61790ba7722b3
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-30 14:37:44 +0600

    alternate approach

--- ps-approach/count-char.js
+++ ps-approach/count-char.js
@@ -2,11 +2,19 @@ function countChar(str) {
   const lowerCasedStr = str.toLowerCase();
   const count = {};
 
-  [...lowerCasedStr].forEach((char) => {
-    if (/[^a-zA-Z0-9]/g.test(char)) return;
+  // [...lowerCasedStr].forEach((char) => {
+  //   if (/[^a-zA-Z0-9]/g.test(char)) return; // Note: Retuen because it's inside a cb function.
+  //   if (count[char]) count[char]++;
+  //   else count[char] = 1;
+  // });
+
+  for (i = 0; i < lowerCasedStr.length; i++) {
+    const char = lowerCasedStr[i];
+
+    if (/[^a-zA-Z0-9]/g.test(char)) continue; // Note: Continue because it's inside a loop!
     if (count[char]) count[char]++;
     else count[char] = 1;
-  });
+  }
 
   return count;
 }
