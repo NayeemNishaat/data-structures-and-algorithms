eb8028203dd03f96b21d4275dff81ff45bdcc86a
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-01 17:49:22 +0600

    remarks added

--- ps-approach/freq-counter.js
+++ ps-approach/freq-counter.js
@@ -11,4 +11,23 @@ function same(arr1, arr2) {
   }
   return true;
 }
-console.log(same([1, 2, 3], [4, 1, 9])); // true
+// console.log(same([1, 2, 3], [4, 1, 9])); // true
+
+// Chapter: Efficient O(n) Frequency Counter Algorithm
+function freqSame(arr1, arr2) {
+  const freqCounter1 = {};
+  const freqCounter2 = {};
+
+  for (const val of arr1) freqCounter1[val] = ++freqCounter1[val] || 1;
+  for (const val of arr2) freqCounter2[val] = ++freqCounter2[val] || 1;
+
+  for (const key in freqCounter1) {
+    if (!(key ** 2 in freqCounter2)) return false;
+    if (freqCounter2[key ** 2] !== freqCounter1[key]) return false;
+  }
+  return true;
+}
+console.log(freqSame([1, 2, 3, 2, 5], [4, 4, 1, 9, 25])); // false
+// console.log(2 in { 2: 3, 6: 4 });
+
+// Remark: In frequency counter algorithm, we can use objects to store the frequency of the arrays. And then we can compare the objects to get the expected result.
