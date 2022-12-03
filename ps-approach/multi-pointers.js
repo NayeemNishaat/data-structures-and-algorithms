e62bda128953329031d748b0e6602a4f0223d46b
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-04 00:14:12 +0600

    solved with mp

--- ps-approach/multi-pointers.js
+++ ps-approach/multi-pointers.js
@@ -1,4 +1,4 @@
-// Inefficient O(n^2) solution
+// Chapter: Inefficient O(n^2) solution
 function sumZero(arr) {
   for (const val of arr) {
     for (const val2 of arr) {
@@ -8,4 +8,20 @@ function sumZero(arr) {
     }
   }
 }
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
+// console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
+
+// Chapter: Efficient O(n) solution
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+
+    if (sum === 0) return [arr[left], arr[right]];
+
+    if (sum > 0) right--;
+    else left++;
+  }
+}
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
