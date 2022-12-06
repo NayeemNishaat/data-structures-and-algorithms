3e607c7dbd54039275874fe95a9ebfd5744411bb
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-06 12:59:51 +0600

    solved with helper and pure recursion

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -18,3 +18,26 @@ function sum(arr) {
   return arr[0] + sum(arr.slice(1));
 }
 console.log(sum([1, 2, 3, 4, 5]));
+
+// Chapter: Collect odd values with helper method recursion
+function collectOddValues(arr) {
+  let result = [];
+  function helper(helperInput) {
+    if (helperInput.length === 0) return;
+    if (helperInput[0] % 2 !== 0) result.push(helperInput[0]);
+    helper(helperInput.slice(1));
+  }
+  helper(arr);
+  return result;
+}
+console.log(collectOddValues([1, 2, 3, 4, 5]));
+
+// Chapter: Collect odd values with pure recursion
+function collectOddValues(arr) {
+  let result = [];
+  if (arr.length === 0) return result;
+  if (arr[0] % 2 !== 0) result.push(arr[0]);
+  result = result.concat(collectOddValues(arr.slice(1)));
+  return result;
+}
+console.log(collectOddValues([1, 2, 3, 4, 5]));
