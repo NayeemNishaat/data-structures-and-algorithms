c8b7063a5aad7bc39b706929d982c27425f617a8
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-09 19:13:58 +0600

    refactored a bit

--- search/string-search.js
+++ search/string-search.js
@@ -18,14 +18,12 @@ function KMPSearch(str, pattern) {
   let j = 0;
 
   for (i = 1; i < pattern.length; ) {
-    if (pattern[i] === pattern[j]) {
-      lps[i] = j + 1;
-      i++, j++;
-    } else {
+    if (pattern[i] === pattern[j]) lps.push(j + 1), i++, j++;
+    else {
       if (j !== 0) j = lps[j - 1];
-      else (lps[i] = j), i++;
+      else lps.push(j), i++;
     }
   }
-  console.log(lps);
+  return lps;
 }
 console.log(KMPSearch("ababcabcabababd", "aabaabaaa"));
