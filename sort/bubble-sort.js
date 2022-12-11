abfa59baaaeed8c81a57c5cedee1cec39ca9446e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-11 23:35:41 +0600

    optimized

--- sort/bubble-sort.js
+++ sort/bubble-sort.js
@@ -1,9 +1,16 @@
 function bubbleSort(arr) {
+  let swap = false;
   for (let i = arr.length - 1; i >= 0; i--) {
+    swap = false;
     for (let j = 0; j < i; j++) {
-      if (arr[i] < arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
+      console.log(arr, arr[i], arr[j]);
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swap = true;
+      }
     }
+    if (!swap) break;
   }
   return arr;
 }
-console.log(bubbleSort([53, 24, 36, 5, 12, 86, 74, 18, 69, 90, 36]));
+console.log(bubbleSort([30, 1, 2, 3, 4, 5, 6, 7, 8]));
