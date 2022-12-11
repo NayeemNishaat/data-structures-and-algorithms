275d5373c6c2422ccbaafa1c22e7014cd5a19506
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-11 17:56:56 +0600

    sorting considering sensitivity and lang

--- sort/built-in-sort.js
+++ sort/built-in-sort.js
@@ -4,6 +4,10 @@ console.log(arr.sort((a, b) => (a > b ? -1 : 1))); // Note: Sorts alphabetically
 console.log(arr.sort().reverse()); // Note: Sorts alphabetically descending order
 console.log(arr.sort((a, b) => b.localeCompare(a))); // Note: Sorts alphabetically descending order
 
+const arr2 = ["a", "á", "b", "c", "A", "Á", "B", "Z"];
+arr.sort((a, b) => b.localeCompare(a, "es", { sensitivity: "base" }));
+console.log(arr2);
+
 console.log(arr.sort((a, b) => a.length - b.length)); // Remark: Sorts by length of the string
 
 const numArr = [12, 63, 3, 12, 45, 28];
