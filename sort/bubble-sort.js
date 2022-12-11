54225c70e85ecd6e40380ad14300d43aad662763
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-11 23:40:41 +0600

    loga removed

--- sort/bubble-sort.js
+++ sort/bubble-sort.js
@@ -3,7 +3,6 @@ function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
     swap = false;
     for (let j = 0; j < i; j++) {
-      console.log(arr, arr[i], arr[j]);
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         swap = true;
