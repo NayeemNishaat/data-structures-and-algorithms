d09cf46b5f7e1d51eb0861db257e72aa5cd1e20d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-11 20:32:24 +0600

    optimized

--- sort/bubble-sort.js
+++ sort/bubble-sort.js
@@ -1,5 +1,5 @@
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
       if (arr[i] < arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
     }
