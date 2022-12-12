985c7262768cd408751876972552b87e2c19bf98
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-12 15:18:23 +0600

    selection sort implemented

---
+++ sort/selection-sort.js
@@ -0,0 +1,11 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+  return arr;
+}
+console.log(selectionSort([12, 23, 24, 46, 476, 33, 1, 4]));
