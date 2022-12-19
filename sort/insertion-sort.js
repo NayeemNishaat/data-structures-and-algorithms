9107e51eabc43be6965ec67dab978b1b95d99074
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-19 17:11:44 +0600

    refactored

--- sort/insertion-sort.js
+++ sort/insertion-sort.js
@@ -1,9 +1,8 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    for (j = i - 1; j >= 0; j--) {
-      if (arr[j] > arr[i]) arr[j + 1] = arr[j];
-    }
-    arr[j + 1] = arr[i];
+    let curr = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > arr[i]; j--) arr[j + 1] = arr[j];
+    arr[j + 1] = curr; // Important: We can't use arr[i] here as arr[i] is already changed in the above loop. So we use curr instead of arr[i]
   }
   return arr;
 }
