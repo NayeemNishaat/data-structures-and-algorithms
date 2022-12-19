af1189a2af6818f65cf74c787feb45769fc67f21
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-12-19 18:32:28 +0600

    commented

--- sort/insertion-sort.js
+++ sort/insertion-sort.js
@@ -1,7 +1,9 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let curr = arr[i];
-    for (var j = i - 1; j >= 0 && arr[j] > curr; j--) arr[j + 1] = arr[j]; // Note: It will not go to last expression if the previous expression is false
+    // let j = i - 1;
+    // for (j; j >= 0 && arr[j] > curr; j--) arr[j + 1] = arr[j];
+    for (var j = i - 1; j >= 0 && arr[j] > curr; j--) arr[j + 1] = arr[j]; // Note: It will not go to last expression if the previous expression is false and will break the loop. var is used intentionally to make j available outside the loop.
     arr[j + 1] = curr; // Important: We can't use arr[i] here as arr[i] is already changed in the above loop. So we use curr instead of arr[i]
   }
   return arr;
