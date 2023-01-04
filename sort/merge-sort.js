9b812ab7605586e522d59beb2db55d5c8663cc0b
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-04 18:39:52 +0600

    simplified

--- sort/merge-sort.js
+++ sort/merge-sort.js
@@ -30,3 +30,19 @@ console.log(mergeSortedArray([1, 3, 5], [0]));
  */
 
 // Part: Approach 2:
+function mergeSortedArray(arr1, arr2) {
+  const arr = [];
+  let i = 0,
+    j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) arr.push(arr1[i]), i++;
+    else arr.push(arr2[j]), j++;
+  }
+
+  while (i < arr1.length) arr.push(arr1[i]), i++;
+  while (j < arr2.length) arr.push(arr2[j]), j++;
+
+  return arr;
+}
+console.log(mergeSortedArray([1, 3, 5], [0]));
