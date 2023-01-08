8968923d537e46fa69d9dda879470f720a9238e5
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-09 03:30:01 +0600

    plain function

--- sort/quick-sort.js
+++ sort/quick-sort.js
@@ -8,13 +8,12 @@ function getPivot(arr, start = 0, end = arr.length - 1) {
   return pivot;
 }
 
-const arr = [-10, 10, 8, 0, 100, 4, 2, 7, 1, 5, 6, 99, 0, 2, 3];
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left >= right) return arr;
   const pivit = getPivot(arr, left, right);
   quickSort(arr, left, pivit - 1);
   quickSort(arr, pivit + 1, right);
   // quickSort(arr, pivit + 1, arr.length); // Important: Can't do that because in the subsequent call we are shrinking the window (setting pivot - 1 as right) but setting it like this it isn't respecting that.
+  return arr;
 }
-quickSort(arr);
-console.log(arr);
+console.log(quickSort([-10, -110]));
