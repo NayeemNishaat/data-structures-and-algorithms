07cf2010c54172cd428715da46b5c85495796e8e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-10 15:51:21 +0600

    implemented get digits helper

--- sort/radix-sort.js
+++ sort/radix-sort.js
@@ -1,4 +1,5 @@
 function getDigit(num, i) {
   // return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
-  return Math.trunc(num / 10 ** i) % 10;
+  return Math.trunc(Math.abs(num) / 10 ** i) % 10; // Important: Math.abs() to get positive number when we put negative number
 }
+console.log(getDigit(235436, 2));
