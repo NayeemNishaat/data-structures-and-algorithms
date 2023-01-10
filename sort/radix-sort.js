784fa252f9f6d9dcdc1a307f5374b1033a4ac7c0
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-10 15:59:50 +0600

    implemented digit count

--- sort/radix-sort.js
+++ sort/radix-sort.js
@@ -3,3 +3,9 @@ function getDigit(num, i) {
   return Math.trunc(Math.abs(num) / 10 ** i) % 10; // Important: Math.abs() to get positive number when we put negative number
 }
 console.log(getDigit(235436, 2));
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+console.log(digitCount(235436));
