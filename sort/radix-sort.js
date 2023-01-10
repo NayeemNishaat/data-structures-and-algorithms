e7c9490e33d23ecb18fdeeac54301f63e14cf0cb
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-10 16:06:55 +0600

    implemented most digits

--- sort/radix-sort.js
+++ sort/radix-sort.js
@@ -6,6 +6,14 @@ console.log(getDigit(235436, 2));
 
 function digitCount(num) {
   if (num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
+  return Math.trunc(Math.log10(Math.abs(num))) + 1;
 }
 console.log(digitCount(235436));
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+}
+console.log(mostDigits([23, 567, 89, 12234324, 90]));
