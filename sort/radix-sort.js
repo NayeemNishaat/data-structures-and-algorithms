46e45cf49fe13108f81b494fa325291a8ae8fecf
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-10 16:15:18 +0600

    commmented out

--- sort/radix-sort.js
+++ sort/radix-sort.js
@@ -2,18 +2,19 @@ function getDigit(num, i) {
   // return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
   return Math.trunc(Math.abs(num) / 10 ** i) % 10; // Important: Math.abs() to get positive number when we put negative number
 }
-console.log(getDigit(235436, 2));
+// console.log(getDigit(235436, 2));
 
 function digitCount(num) {
   if (num === 0) return 1;
   return Math.trunc(Math.log10(Math.abs(num))) + 1;
 }
-console.log(digitCount(235436));
+// console.log(digitCount(235436));
 
 function mostDigits(nums) {
   let maxDigits = 0;
   for (let i = 0; i < nums.length; i++) {
     maxDigits = Math.max(maxDigits, digitCount(nums[i]));
   }
+  return maxDigits;
 }
-console.log(mostDigits([23, 567, 89, 12234324, 90]));
+// console.log(mostDigits([23, 567, 89, 977446, 90]));
