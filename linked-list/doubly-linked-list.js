27ddac90938dba28615797750554cb53e16c26ae
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-17 03:36:46 +0600

    implemented push for dll

--- linked-list/doubly-linked-list.js
+++ linked-list/doubly-linked-list.js
@@ -12,4 +12,21 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      (this.head = this.tail = newNode), this.length++;
+      return;
+    }
+
+    (this.tail.next = newNode), (newNode.prev = this.tail);
+    (this.tail = newNode), this.length++;
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+
+console.log(JSON.stringify(list, null, 2));
