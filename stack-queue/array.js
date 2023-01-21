08e45c968a835ec41d2475f6db46e32c9ab76490
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-21 14:25:04 +0600

    implemented quque with array

--- stack-queue/array.js
+++ stack-queue/array.js
@@ -1,6 +1,6 @@
+// Part: Stack with push/pop
 const stack = [];
 
-// Part: Stack with push/pop
 stack.push(1);
 stack.push(2);
 stack.push(3);
@@ -15,3 +15,21 @@ stack.unshift(3);
 stack.shift();
 stack.shift();
 stack.shift();
+
+// Part: Queue with push/shift
+const queue = [];
+
+queue.push(1);
+queue.push(2);
+queue.push(3);
+queue.shift();
+queue.shift();
+queue.shift();
+
+// Part: Queue with unshift/pop
+queue.unshift(1);
+queue.unshift(2);
+queue.unshift(3);
+queue.pop();
+queue.pop();
+queue.pop();
