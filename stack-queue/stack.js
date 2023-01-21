9c548ce50c07e6e3f8ef2bd81408ec7ce7c29305
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-21 09:24:41 +0600

    implemented stack with array

---
+++ stack-queue/stack.js
@@ -0,0 +1,14 @@
+const stack = [];
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.pop();
+stack.pop();
+stack.pop();
+
+stack.unshift(1);
+stack.unshift(2);
+stack.unshift(3);
+stack.shift();
+stack.shift();
+stack.shift();
