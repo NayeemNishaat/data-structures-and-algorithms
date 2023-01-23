291a4da0d644b8cdd5b676f469d708368d70c677
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-24 05:18:33 +0600

    implemented bst insert

--- tree/bst.js
+++ tree/bst.js
@@ -11,5 +11,28 @@ class BST {
     this.root = null;
   }
 
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) this.root = newNode;
+
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+
+      if (value < current.value) {
+        if (current.left === null) current.left = newNode;
+        current = current.left;
+      } else {
+        if (current.right === null) current.right = newNode;
+        current = current.right;
+      }
+    }
+  }
 }
+
+const tree = new BST();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+
+console.log(tree);
