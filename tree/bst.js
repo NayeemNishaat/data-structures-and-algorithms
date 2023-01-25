89ecd42181ad631ca06143fb2cdead8ca119d83d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-26 05:14:59 +0600

    implemented bst find

--- tree/bst.js
+++ tree/bst.js
@@ -37,11 +37,33 @@ class BST {
       }
     }
   }
+
+  find(value) {
+    if (!this.root) return false;
+
+    let currentRoot = this.root;
+    while (true) {
+      if (currentRoot.value === value) return true;
+      if (value < currentRoot.value) {
+        if (!currentRoot.left) return false;
+        if (value === currentRoot.left.value) return true;
+        currentRoot = currentRoot.left;
+      } else {
+        if (!currentRoot.right) return false;
+        if (value === currentRoot.right.value) return true;
+        currentRoot = currentRoot.right;
+      }
+    }
+  }
 }
 
 const tree = new BST();
 tree.insert(10);
 tree.insert(5);
 tree.insert(13);
-
+tree.insert(20);
+tree.insert(2);
+tree.insert(34);
+tree.insert(0);
+console.log(tree.find(34));
 console.log(tree);
