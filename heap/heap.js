c32da55fc9a752ed8be181f159e702068172e2e6
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-29 13:00:41 +0600

    implemented extract max

--- heap/heap.js
+++ heap/heap.js
@@ -16,6 +16,28 @@ class maxBinaryHeap {
       parentIdx = Math.floor((idx - 1) / 2);
     }
   }
+
+  extractMax() {
+    const vals = this.values;
+    [vals[0], vals[vals.length - 1]] = [vals[vals.length - 1], vals[0]];
+    const extracted = vals.pop();
+
+    let parentIdx = 0,
+      left = 2 * parentIdx + 1,
+      right = 2 * parentIdx + 2;
+
+    while (vals[left] > vals[parentIdx] || vals[right] > vals[parentIdx]) {
+      if (vals[left] > vals[right])
+        ([vals[left], vals[parentIdx]] = [vals[parentIdx], vals[left]]),
+          (parentIdx = left);
+      else
+        ([vals[right], vals[parentIdx]] = [vals[parentIdx], vals[right]]),
+          (parentIdx = right);
+
+      (left = 2 * parentIdx + 1), (right = 2 * parentIdx + 2);
+    }
+    return extracted;
+  }
 }
 
 const heap = new maxBinaryHeap();
@@ -29,4 +51,6 @@ heap.insert(12);
 heap.insert(55);
 heap.insert(1);
 
+console.log(heap);
+heap.extractMax();
 console.log(heap);
