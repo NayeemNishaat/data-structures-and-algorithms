7e13a91972e1050aa717066b6afa591d6f7e3de6
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-01-30 04:48:23 +0600

    implemented enqueue priority queue

---
+++ heap/priorityQueue.js
@@ -0,0 +1,57 @@
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class minBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(element, priority) {
+    const newNode = new Node(element, priority);
+    const values = this.values;
+    values.push(newNode);
+    let idx = values.length - 1,
+      parentIdx = Math.floor((idx - 1) / 2);
+
+    while (values[idx].priority < values[parentIdx]?.priority) {
+      [values[idx], values[parentIdx]] = [values[parentIdx], values[idx]];
+
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+  }
+
+  extractMax() {
+    const vals = this.values;
+    [vals[0], vals[vals.length - 1]] = [vals[vals.length - 1], vals[0]];
+    const extracted = vals.pop();
+
+    let parentIdx = 0,
+      left = 2 * parentIdx + 1,
+      right = 2 * parentIdx + 2;
+
+    while (vals[left] > vals[parentIdx] || vals[right] > vals[parentIdx]) {
+      if (vals[left] > vals[right] || !vals[right])
+        ([vals[left], vals[parentIdx]] = [vals[parentIdx], vals[left]]),
+          (parentIdx = left);
+      else
+        ([vals[right], vals[parentIdx]] = [vals[parentIdx], vals[right]]),
+          (parentIdx = right);
+
+      (left = 2 * parentIdx + 1), (right = 2 * parentIdx + 2);
+    }
+    return extracted;
+  }
+}
+
+const heap = new minBinaryHeap();
+
+heap.enqueue("Help", 6);
+heap.enqueue("Urgent", 3);
+heap.enqueue("Immediate", 2);
+
+console.log(heap.values);
