5b004617a8a41b2031177167134c38c8908ae1ad
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-01 04:58:46 +0600

    implemented add edge

--- graph/graph.js
+++ graph/graph.js
@@ -4,11 +4,20 @@ class graph {
   }
 
   addVertex(vertex) {
-    this.adjacencyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 }
 
 const g = new graph();
 
-g.addVertex("abc");
+g.addVertex("dhaka");
+g.addVertex("tokyo");
+g.addVertex("aspin");
+g.addEdge("dhaka", "tokyo");
+g.addEdge("dhaka", "aspin");
 console.log(g.adjacencyList);
