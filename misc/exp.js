5e5022d4f4d918121b83c83b4ef3f25fa7d696f6
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-12 22:51:58 +0600

    refactored

--- misc/exp.js
+++ misc/exp.js
@@ -1,31 +1,19 @@
-// Workout plan to burn calories every day
 function caloriesBurned(cals, d, min, max) {
   const newarr = [];
   let total = 0;
-  // newarr.push(cals.sbustr(0, d));
-  // console.log(newarr);
-  for (let i = 0; i < cals.length + d; i += d) {
-    if (i + d <= cals.length + 1) {
-      newarr.push(cals.slice(i, i + d));
-    }
+
+  for (let i = 0; i < cals.length; i += d) {
+    newarr.push(cals.slice(i, i + d));
   }
-  console.log(newarr);
 
   newarr.forEach((element) => {
     let temp = 0;
-    element.forEach((el) => {
-      temp += el;
-    });
-    // console.log(temp);
-    if (temp < min) {
-      total -= 1;
-    }
-    if (temp > max) {
-      total += 1;
-    }
+    element.forEach((el) => (temp += el));
+
+    if (temp < min) total -= 1;
+    if (temp > max) total += 1;
   });
-  console.log(total);
   return total;
 }
 
-caloriesBurned([1, 2, 3, 4, 5], 3, 3, 3);
+console.log(caloriesBurned([1, 2, 3, 4, 5, 6, 7], 5, 3, 3));
