4d766180e5b762f41562ada5c379e06de2432856
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-11 03:03:50 +0600

    exp

--- misc/exp.js
+++ misc/exp.js
@@ -1,25 +1,18 @@
-setTimeout(() => {
-  const size = (audio.duration * 24000) / 1000;
-  $("#audioFiles").append(`
-    <div class="upload-box">
-      <ul>
-        <li>
-          <div class="upload-file-name">
-            <p>AI Generated Audio</p>
-            <span>${size} KB</span>
-            ${audio}
-          </div>
-        </li>
-        <li>
-          <div class="progress-box">
-            <span class="progressfill"></span>
-          </div>
-          <p>100%</p>
-        </li>
-        <li>
-          <img src="/assets/createCampaign/images/delete.png" onclick="removeAudioFile(${i})" />
-        </li>
-      </ul>
-    </div>
-`);
-}, 1000);
+const arr = [1, 2, 3, 4];
+
+(async () => {
+  try {
+    try {
+      arr.forEach((el) => {
+        console.log(el);
+
+        if (el == 2) throw new Error("Faul");
+      });
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  } catch (err) {
+    console.log(err, 78);
+  }
+})();
