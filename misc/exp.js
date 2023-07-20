5affb790da0e545be9124d5a9eefdf53895a7dc9
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-20 15:52:59 +0600

    testing

--- misc/exp.js
+++ misc/exp.js
@@ -1,18 +1,11 @@
-const arr = [1, 2, 3, 4];
+const text =
+  "\n" +
+  ">Welcome to NBM, the best pizza delivery service in town! Our unique value proposition is that we offer 10% off all orders. So don't wait, come visit and enjoy! \n" +
+  "\n" +
+  "We also have a website set up where you can order quickly and easily, and check out all the delicious options we have available. So if you're looking for a delicious, convenient pizza delivery service, then NBM is the perfect choice for you! \n" +
+  "\n" +
+  "We want to remind you that with NBM, you get 10% off all orders, so don't miss out. Visit our website today and see all the amazing options we have available. \n" +
+  "\n" +
+  "This podcast ad is brought to you with some catchy background music to make sure you remember all the details. So don't wait, come visit and enjoy the amazing pizza delivery service at NBM!";
 
-(async () => {
-  try {
-    try {
-      arr.forEach((el) => {
-        console.log(el);
-
-        if (el == 2) throw new Error("Faul");
-      });
-    } catch (error) {
-      console.log(error);
-      return;
-    }
-  } catch (err) {
-    console.log(err, 78);
-  }
-})();
+text.slice(text.indexOf(">") + 1);
