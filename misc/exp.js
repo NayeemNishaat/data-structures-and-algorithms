32278829c2feb630eba6f397d0fbb149bfbc4ad8
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-08-02 12:35:49 +0600

    working with async callbacks

--- misc/exp.js
+++ misc/exp.js
@@ -1,11 +1,28 @@
-const text =
-  "\n" +
-  ">Welcome to NBM, the best pizza delivery service in town! Our unique value proposition is that we offer 10% off all orders. So don't wait, come visit and enjoy! \n" +
-  "\n" +
-  "We also have a website set up where you can order quickly and easily, and check out all the delicious options we have available. So if you're looking for a delicious, convenient pizza delivery service, then NBM is the perfect choice for you! \n" +
-  "\n" +
-  "We want to remind you that with NBM, you get 10% off all orders, so don't miss out. Visit our website today and see all the amazing options we have available. \n" +
-  "\n" +
-  "This podcast ad is brought to you with some catchy background music to make sure you remember all the details. So don't wait, come visit and enjoy the amazing pizza delivery service at NBM!";
+async function c() {
+  // throw new Error("Err");
+}
 
-text.slice(text.indexOf(">") + 1);
+async function b() {
+  await c();
+}
+
+async function a() {
+  try {
+    await b();
+
+    await x(async () => {
+      try {
+        throw new Error("XXX");
+      } catch (error) {
+        throw new Error(error);
+      }
+    });
+  } catch (error) {
+    console.log(22, error);
+  }
+}
+a();
+
+async function x(fn) {
+  await fn();
+}
