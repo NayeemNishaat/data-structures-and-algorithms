ce366720e658ea9a3cdbd5b61e1236c8d3a946ff
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-10-25 15:47:06 +0600

    updated

--- misc/week-range.js
+++ misc/week-range.js
@@ -1,27 +1,34 @@
 const startDate = new Date("2023-10-20 00:00:00Z");
-const endDate = new Date("2023-10-17 23:59:59Z");
-const interval = 5;
+const endDate = new Date("2023-10-27 23:59:59Z");
+const interval = 7;
 
 function getCurrentWeekRange(startDate, endDate, interval) {
   if (!interval) return { startDate, endDate };
 
   endDate = endDate.getTime();
+  startDate = startDate.getTime();
   const currentDate = new Date().getTime();
-  console.log(new Date(currentDate));
+  // console.log(new Date(currentDate));
   let tempDate = new Date(startDate);
   let nextDate = tempDate.setDate(tempDate.getDate() + interval);
-  // console.log(typeof nextDate);
+
+  if (endDate <= startDate)
+    throw new Error("End date must be greater than start date.");
+
   while (nextDate < endDate) {
-    if (currentDate < nextDate) {
-      return { startDate, endDate: new Date(nextDate - 1) };
-    } else {
-      startDate = new Date(nextDate);
+    if (currentDate < nextDate)
+      return {
+        startDate: new Date(startDate),
+        endDate: new Date(nextDate - 1)
+      };
+    else {
+      startDate = nextDate;
       tempDate = new Date(nextDate);
       nextDate = tempDate.setDate(tempDate.getDate() + interval);
     }
   }
 
-  return { startDate, endDate: new Date(endDate) };
+  return { startDate: new Date(startDate), endDate: new Date(endDate) };
 }
 
 console.log(getCurrentWeekRange(startDate, endDate, interval));
