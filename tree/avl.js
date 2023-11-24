f2af81d411adbd9df1749175398136526705f472
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-11-25 03:45:33 +0600

    work on avl insert

--- tree/avl.js
+++ tree/avl.js
@@ -3,7 +3,7 @@ class Node {
     this.value = value;
     this.left = null;
     this.right = null;
-    this.bf = 0;
+    this.height = 1;
   }
 }
 
@@ -12,77 +12,28 @@ class AVL {
     this.root = root || null;
   }
 
-  getHeight() {}
-
-  balanceFactor() {
-    function visit(n) {
-      let height = 1;
-
-      function traverse(node) {
-        if (!node) return 0;
-
-        if (node.left) {
-          height++;
-          traverse(node.left);
-        }
-
-        if (node.right) {
-          height++;
-          traverse(node.right);
-        }
-        return height;
-      }
-
-      const leftHeight = traverse(n.left);
-      const rightHeight = traverse(n.right);
-      n.bf = leftHeight - rightHeight;
-
-      if (n.left) visit(n.left);
-      if (n.right) visit(n.right);
-    }
-    visit(this.root);
+  getHeight(node) {
+    if (!node) return 0;
+    return node.height;
   }
 
-  insert(value) {
-    const node = new Node(value);
+  getBalanceFactor() {}
 
-    if (!this.root) {
-      this.root = node;
-      return;
-    }
+  insert(value, node = this.root) {
+    if (!node) {
+      return new Node(value);
+    } else if (value < node.value) node.left = this.insert(value, node.left);
+    else node.right = this.insert(value, node.right);
 
-    let currentRoot = this.root;
-    while (true) {
-      if (value < currentRoot.value) {
-        if (!currentRoot.left) {
-          currentRoot.left = node;
-
-          this.balanceFactor();
-
-          return;
-        }
-        currentRoot = currentRoot.left;
-      } else {
-        if (value > currentRoot.value) {
-          if (!currentRoot.right) {
-            currentRoot.right = node;
-            return;
-          }
-        }
-        currentRoot = currentRoot.right;
-      }
-    }
+    node.height =
+      1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+    return node;
   }
 }
 
-const node = new Node(20);
-node.left = new Node(10);
-node.right = new Node(40);
-node.right.left = new Node(25);
-node.right.left.right = new Node(30);
-
-const avl = new AVL(node);
-
-avl.balanceFactor();
+const avl = new AVL(new Node(50));
+avl.insert(20);
+avl.insert(10);
+avl.insert(100);
 
 console.dir(avl, { depth: null });
