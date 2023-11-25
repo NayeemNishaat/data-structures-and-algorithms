1f7a5ebc4f3e56c56fcdac00ad54719b879ef576
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-11-25 15:59:40 +0600

    fixing

--- tree/avl.js
+++ tree/avl.js
@@ -22,6 +22,30 @@ class AVL {
     return this.getHeight(node.left) - this.getHeight(node.right);
   }
 
+  rightRotate(node) {
+    const newNode = node.left;
+    node.left = newNode.right;
+    newNode.right = node;
+
+    node.height = this.getHeight(node.left) - this.getHeight(node.right);
+    newNode.height =
+      this.getHeight(newNode.left) - this.getHeight(newNode.right);
+
+    return newNode;
+  }
+
+  leftRotate(node) {
+    const newNode = node.right;
+    node.right = newNode.left;
+    newNode.left = node;
+
+    node.height = this.getHeight(node.left) - this.getHeight(node.right);
+    newNode.height =
+      this.getHeight(newNode.left) - this.getHeight(newNode.right);
+
+    return newNode;
+  }
+
   insert(value, node = this.root) {
     if (!node) {
       return new Node(value);
@@ -32,16 +56,36 @@ class AVL {
       1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
 
     const balanceFactor = this.getBalanceFactor(node);
-    console.log(balanceFactor, node);
+
+    if (balanceFactor > 1) {
+      if (value < node.left.value)
+        return this.rightRotate(node); // Note: Do right rotation
+      else {
+        // Note: Do left then right rotation
+        node.left = this.leftRotate(node.left);
+        return this.rightRotate(node);
+      }
+    }
+
+    if (balanceFactor < -1) {
+      if (value > node.right.value) {
+        return this.leftRotate(node); // Note: Do left rotation
+      } else {
+        // Remark: Else do right then left
+        node.right = this.rightRotate(node.right);
+        return this.leftRotate(node);
+      }
+    }
 
     return node;
   }
 }
 
-const avl = new AVL(new Node(20));
-avl.insert(10);
-avl.insert(40);
-avl.insert(25);
+const avl = new AVL(new Node(10));
+avl.insert(20);
 avl.insert(30);
+avl.insert(25);
+avl.insert(27);
+avl.insert(29);
 
-// console.dir(avl, { depth: null });
+console.dir(avl, { depth: null });
