3470df726528abcf3e82f4d64658513811e8e230
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2023-12-06 03:25:04 +0600

    update

---
+++ misc/loop-promise.js
@@ -0,0 +1,21 @@
+(async () => {
+  async function ab(i) {
+    if (i === 2) throw new Error("i is 3");
+  }
+
+  try {
+    for (let i = 0; i < 5; i++) {
+      // Important: If any error occurs inside the loop, the loop will break. So we should handle error inside the loop to keep it going in case of any potential error.
+      try {
+        await ab(i);
+        console.log(i);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    console.log("End");
+  } catch (err) {
+    console.log(err);
+  }
+})();
